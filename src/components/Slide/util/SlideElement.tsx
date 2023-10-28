712cb915fa98655bd41e1835bb0fa8f6c6ac4fea
Yousef <yom@crossworkers.com>
2023-10-28 06:57:59 +0200

    resize logic

--- src/components/Slide/util/SlideElement.tsx
+++ src/components/Slide/util/SlideElement.tsx
@@ -9,6 +9,7 @@ import {
 	updateSlideElement,
 } from "../../../store/actions/actions";
 import { useDebounce } from "../../../util/Debounce";
+import ResizeIndicators from "./util/ResizeIndicators";
 interface ISlideElementProps {
 	slideElement: ISlideElement;
 	navigation: boolean;
@@ -276,64 +277,7 @@ const SlideElement = (props: ISlideElementProps) => {
 			})}
 		>
 			{!props.navigation && (
-				<>
-					<div
-						className="resize-indicator top-left bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-nwse-resize -left-1 -top-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator middle-left bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-ew-resize -left-1 top-[calc(50%-4px)]"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator top-middle bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-ns-resize left-[calc(50%-4px)] -top-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator bottom-left bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-nesw-resize -left-1 -bottom-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator top-right bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-nesw-resize -right-1 -top-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator bottom-middle bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-ns-resize left-[calc(50%-4px)] -bottom-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator middle-right bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-ew-resize -right-1 top-[calc(50%-4px)]"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-					<div
-						className="resize-indicator bottom-right bg-neutral-800 absolute z-1 w-2 h-2 rounded-full cursor-nwse-resize -right-1 -bottom-1"
-						onMouseDown={(event) => {
-							event.stopPropagation();
-							handleResize(event);
-						}}
-					/>
-				</>
+				<ResizeIndicators resizeFunction={handleResize} />
 			)}
 			<ContentEditable
 				className={`focus:outline-none w-full ${
