375c8cf5a6bb5bf1dc495a6be4129d3bd9114dfa
Yousef <yom@crossworkers.com>
2023-10-29 17:49:46 +0200

    init custom elements

--- src/logic/SlideElement.ts
+++ src/logic/SlideElement.ts
@@ -3,11 +3,13 @@ import Drawable from "./Drawable";
 class SlideElement extends Drawable implements ISlideElement {
 	private _id: Readonly<string>;
 	content: string;
+	type: string;
 	constructor(slideElement?: ISlideElement) {
 		super(slideElement?.position, slideElement?.size);
 		this._id =
 			Date.now().toString(36) + Math.random().toString(36).substr(2);
 		this.content = slideElement?.content || "";
+		this.type = slideElement?.type || "text";
 	}
 	get id(): Readonly<string> {
 		return this._id;
